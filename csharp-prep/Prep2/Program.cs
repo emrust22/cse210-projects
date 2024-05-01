fbedca52892b4f6473129ca445f9f1a670c1ea55
emrust22 <emilyrust22@gmail.com>
2024-05-01 06:05:18 -0700

    Completed prep2 code.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string gradePercentInput = Console.ReadLine();
+        int grade = int.Parse(gradePercentInput);
+        
+        string letter = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >=80 && grade <= 89)
+        {
+            letter = "B";
+        }
+        else if (grade >=70 && grade <= 79)
+        {
+            letter = "C";
+        }
+        else if (grade >=60 && grade <= 69)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+        if (grade >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you failed.");
+        }
     }
 }
