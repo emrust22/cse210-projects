56c56888a7495da217a29e6277dd639ffaadcd00
emrust22 <emilyrust22@gmail.com>
2024-05-04 08:12:42 -0700

    Completed L02 Assignment

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Digital AdOps Specialist";
+        job1._company = "Premion";
+        job1._startYear = 2021;
+        job1._endYear = 2024;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Paid Media Specialist";
+        job2._company = "Digital Natives";
+        job2._startYear = 2024;
+        job2._endYear = 2024;
+
+        Resume myResume = new Resume();
+        myResume._name = "Emily Rust";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
